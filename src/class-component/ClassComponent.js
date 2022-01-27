c6ff781359a7bdcd079fd8c3336d44d3d46826bb
Irvin Lopez <irvin_21_95@hotmail.com>
2022-01-26 21:08:08 -0700

    class and function component

---
+++ src/class-component/ClassComponent.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+export class ClassComponent extends Component {
+  render() {
+    const { name, age } = this.props;
+    return (
+      <div className="App">
+        <h1>Class Component</h1>
+        <h2>Nombre: {name}</h2>
+        <h2>Edad: {age}</h2>
+      </div>
+    );
+  }
+}
